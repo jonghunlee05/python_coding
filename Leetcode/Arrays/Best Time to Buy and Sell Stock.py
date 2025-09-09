2e87b9a2a9d80491022dfdcc556dbb8c9ec3a9f1
Jonghun Lee <jonghunlee.student@gmail.com>
2025-09-09 21:51:37 +0100

    Add LeetCode Array solutions: Two Sum and Best Time to Buy and Sell Stock

---
+++ Leetcode/Arrays/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max = 0
+        for i in range(0,len(prices)):
+            for j in range(i + 1, len(prices)):
+                if prices[j] - prices [i] <= 0:
+                    break
+                if prices[j] - prices[i] > max:
+                    max = prices[j] - prices[i]
+
+        return max
