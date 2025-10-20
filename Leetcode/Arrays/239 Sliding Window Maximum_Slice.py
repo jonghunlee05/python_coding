5b3890c7ffae300717259e35c7a97b1097408f3c
Jonghun Lee <jonghunlee.student@gmail.com>
2025-10-20 13:50:12 +0100

    Add 215 Kth Largest Element solution and update 239 Sliding Window Maximum

--- Leetcode/Arrays/239 Sliding Window Maximum_Slice.py
+++ Leetcode/Arrays/239 Sliding Window Maximum_Slice.py
@@ -15,14 +15,13 @@ Window position                Max
 we have to use deque. 
 
 
-but what we have to do is, don't change the nums. 
-make a deque that has k length. 
-
-1. just keep the index until max appears. 
-2. once max appears, unless the next max appears or index out of range, 
-keep adding the new indexs to the queue. 
-3. once new max appears, pop all of the queue and put the new max_idx only. 
-4. so, let the first element in the queue be the max. 
+1. first, check if the index is always within the window range k. 
+2. then, start your operations. 
+3. make sure you keep adding all of the indexes to the queue. 
+4. but the key part is, once you reach an index where it was maximum then any others, just remove all of the previous indexes from the deque. 
+5. adding values to results array -> only when within range, and 2 conditions:
+    1. if the index value gives the max then the others. 
+    2. ******if the index value is already out of the scope *******
 """
 
 from collections import deque
@@ -30,27 +29,26 @@ from collections import deque
 nums = [1,-1]
 k = 1
 
-q = deque([0])
-max_nums = []
 
+q = deque()      # will hold indices
+res = []
 
-for i in range(len(nums)):
-    
-    if len(q) == k: 
-        q.clear()
-        q.append(i)
-        print(q)
-    
-    if i + 1 < k or nums[i] > nums[q[0]]: # new max occured!
-        q.clear()
-        q.append(i)
-        print(q)
-    else: 
-        q.append(i)
-        print(q)
-    
-    if i + 1 >= k: 
-        max_nums.append(nums[q[0]])
+for i, num in enumerate(nums):
+    # 1. Remove indices out of current window
+    if q and q[0] <= i - k:
+        q.popleft()
+
+    # 2. Remove smaller numbers from the back
+    while q and nums[q[-1]] < num:
+        q.pop()
+
+    # 3. Add current index
+    q.append(i)
+
+    # 4. Append current max (from front)
+    if i >= k - 1:
+        res.append(nums[q[0]])
+
     
 
-print(max_nums)
+print(res)
